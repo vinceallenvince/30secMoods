305ff62163188f898221636e1ce69b10159fa29c
Vince Allen <vince@spotify.com>
2014-06-20 12:59:15 -0400

    Updates port.

--- app.js
+++ app.js
@@ -10,10 +10,11 @@
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
+var port = 80;
 
 var client_id = '03ffe0cac0a0401aa6673c3cf6d02ced'; // Your client id
 var client_secret = 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:80/callback'; // Your redirect uri
 
 var echojs = require('echojs');
 
@@ -155,8 +156,8 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-server.listen(8888);
+console.log('Listening on 80');
+server.listen(80);
 
 //
 
