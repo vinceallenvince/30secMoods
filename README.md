60f1411e13474720b37eef3feeb8db3beb5010ef
Vince Allen <vince@spotify.com>
2014-06-20 12:56:45 -0400

    Removes unnecessary node module.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-### 30sec Moods
+# 30sec Moods
 
 This node app uses both the [Echo Nest](http://developer.echonest.com) and [Spotify Web](http://developer.spotify.com) APIs to generate 30 second audio previews based on Echo Nest moods.
+
+### Install and run
+
+```html
+git clone git@ghe.spotify.net:vince/30secMoods.git
+cd 30secMoods
+npm install
+node app
+```
--- app.js
+++ app.js
@@ -10,7 +10,6 @@
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
-var webapi = require('spotify-webapi');
 
 var client_id = '03ffe0cac0a0401aa6673c3cf6d02ced'; // Your client id
 var client_secret = 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
