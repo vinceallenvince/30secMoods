8ea11d65c44bc224b8386d894df262729e2f99dd
Vince Allen <vince@spotify.com>
2014-06-19 21:24:53 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,169 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+ */
+
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var webapi = require('spotify-webapi');
+
+var client_id = '03ffe0cac0a0401aa6673c3cf6d02ced'; // Your client id
+var client_secret = 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+var echojs = require('echojs');
+
+var echo = echojs({
+  key: 'UQZFWAPECBUEJSNX6'
+});
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+io.on('connection', function (socket) {
+  console.log('connected!');
+
+  socket.on('getArtistsByMood', function(data) {
+
+      echo('artist/search').get({
+        format: 'json',
+        genre: 'blues',
+        mood: data.mood,
+        results: 5,
+        callback: bustClientCache()
+      }, function (err, json) {
+        if (err) {
+          console.error('Received error code: %s', err.status);
+          return;
+        }
+
+        var artists = json.response.artists;
+        var l = artists.length;
+        var artist_seeds = [];
+        for (var i = 0; i < l; i++) {
+          artist_seeds.push(artists[i].id);
+        }
+
+        echo('playlist/static').get({
+          format: 'json',
+          type: 'artist',
+          artist: artist_seeds,
+          results: 20,
+          song_selection: data.valence ? data.valence == 1 ? 'valence-top' : 'valence-bottom' : false,
+          bucket: ['id:spotifyv2-ZZ', 'tracks'],
+          callback: bustClientCache()
+        }, function (err, json) {
+          if (err) {
+            console.error('Received error code: %s', err.status);
+            return;
+          }
+          socket.emit('gotTracks', {data: json.response});
+        });
+      });
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/login', function(req, res) {
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  var code = req.query.code;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code',
+      client_id: client_id,
+      client_secret: client_secret
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+      var options = {
+        url: 'https://api.spotify.com/v1/me',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        json: true
+      };
+
+      // use the access token to access the Spotify Web API
+      request.get(options, function(error, response, body) {
+        console.log(body);
+      });
+
+      // we can also pass the token to the browser to make requests from there
+      res.redirect('/#' +
+        querystring.stringify({
+          access_token: access_token,
+          refresh_token: refresh_token
+        }));
+    }
+  });
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+console.log('Listening on 8888');
+server.listen(8888);
+
+//
+
+function getRandomNumber(low, high, flt) {
+  if (flt) {
+    return Math.random()*(high-(low-1)) + low;
+  }
+  return Math.floor(Math.random()*(high-(low-1))) + low;
+};
+
+
+function bustClientCache() {
+  return getRandomNumber(0, 100000);
+}
---
+++ public/scripts/moods.js
@@ -0,0 +1,205 @@
+var moods = {
+  moods: [
+    {
+      name: 'dark', val: 1, valence: -1
+    },
+    {
+      name: 'light', val: 0.4650937918, valence: false
+    },
+    {
+      name: 'strange', val: 0.3160948711, valence: false
+    },
+    {
+      name: 'sweet', val: 0.3030742144, valence: 1
+    },
+    {
+      name: 'energetic', val: 0.3010213381, valence: false
+    },
+    {
+      name: 'haunting', val: 0.2917349176, valence: -1
+    },
+    {
+      name: 'cool', val: 0.2910981985, valence: false
+    },
+    {
+      name: 'cold', val: 0.2627918501, valence: false
+    },
+    {
+      name: 'lively', val: 0.2564993669, valence: false
+    },
+    {
+      name: 'smooth', val: 0.2434608118, valence: false
+    },
+    {
+      name: 'sentimental', val: 0.2370039053, valence: false
+    },
+    {
+      name: 'funky', val: 0.236835681, valence: false
+    },
+    {
+      name: 'reflective', val: 0.231586263, valence: false
+    },
+    {
+      name: 'intense', val: 0.2315105017, valence: false
+    },
+    {
+      name: 'gloomy', val: 0.229729228, valence: -1
+    },
+    {
+      name: 'sad', val: 0.2295154028, valence: -1
+    },
+    {
+      name: 'warm', val: 0.2290323063, valence: false
+    },
+    {
+      name: 'ambient', val: 0.227639298, valence: false
+    },
+    {
+      name: 'dreamy', val: 0.2249091362, valence: false
+    },
+    {
+      name: 'whimsical', val: 0.2210481713, valence: 1
+    },
+    {
+      name: 'sexy', val: 0.2181591615, valence: false
+    },
+    {
+      name: 'epic', val: 0.2180848423, valence: false
+    },
+    {
+      name: 'gentle', val: 0.2134895865, valence: false
+    },
+    {
+      name: 'eerie', val: 0.2130739488, valence: -1
+    },
+    {
+      name: 'ominous', val: 0.2111263624, valence: -1
+    },
+    {
+      name: 'humorous', val: 0.2011348021, valence: 1
+    },
+    {
+      name: 'calming', val: 0.1994305633, valence: false
+    },
+    {
+      name: 'disturbing', val: 0.1926953022, valence: -1
+    },
+    {
+      name: 'futuristic', val: 0.1861822967, valence: false
+    },
+    {
+      name: 'mellow', val: 0.1830903353, valence: false
+    },
+    {
+      name: 'happy', val: 0.1767771473, valence: 1
+    },
+    {
+      name: 'groovy', val: 0.1756404862, valence: false
+    },
+    {
+      name: 'elegant', val: 0.1736745308, valence: false
+    },
+    {
+      name: 'poignant', val: 0.1577234703, valence: false
+    },
+    {
+      name: 'cheerful', val: 0.1576994657, valence: 1
+    },
+    {
+      name: 'quiet', val: 0.1481643177, valence: false
+    },
+    {
+      name: 'carefree', val: 0.1382789318, valence: false
+    },
+    {
+      name: 'dramatic', val: 0.1359121232, valence: false
+    },
+    {
+      name: 'manic', val: 0.122236751, valence: false
+    },
+    {
+      name: 'relax', val: 0.1220996639, valence: false
+    },
+    {
+      name: 'mystical', val: 0.1210303245, valence: false
+    },
+    {
+      name: 'angry', val: 0.1196954019, valence: false
+    },
+    {
+      name: 'fun', val: 0.119361951, valence: 1
+    },
+    {
+      name: 'rowdy', val: 0.1157236945, valence: false
+    },
+    {
+      name: 'angst-ridden', val: 0.1117215861, valence: false
+    },
+    {
+      name: 'playful', val: 0.1105586324, valence: 1
+    },
+    {
+      name: 'industrial', val: 0.09950487951, valence: false
+    },
+    {
+      name: 'peaceful', val: 0.0970263944, valence: false
+    },
+    {
+      name: 'spiritual', val: 0.09561601404, valence: false
+    },
+    {
+      name: 'intimate', val: 0.09355273111, valence: false
+    },
+    {
+      name: 'soothing', val: 0.09057899382, valence: 1
+    },
+    {
+      name: 'aggressive', val: 0.09002335389, valence: false
+    },
+    {
+      name: 'hypnotic', val: 0.08322234678, valence: false
+    },
+    {
+      name: 'passionate', val: 0.08313562205, valence: false
+    },
+    {
+      name: 'meditation', val: 0.07980897728, valence: false
+    },
+    {
+      name: 'harsh', val: 0.07207930336, valence: false
+    },
+    {
+      name: 'bouncy', val: 0.06850026946, valence: false
+    },
+    {
+      name: 'sophisticated', val: 0.06735039299, valence: false
+    },
+    {
+      name: 'spacey', val: 0.06665403044, valence: false
+    },
+    {
+      name: 'rebellious', val: 0.05428449343, valence: false
+    },
+    {
+      name: 'complex', val: 0.04059033064, valence: false
+    },
+    {
+      name: 'joyous', val: 0.02833895622, valence: 1
+    },
+    {
+      name: 'melancholia', val: 0.0190609062, valence: false
+    },
+    {
+      name: 'enthusiastic', val: -0.01232492876, valence: 1
+    },
+    {
+      name: 'theater', val: -0.01347482595, valence: false
+    },
+    {
+      name: 'gleeful', val: -0.02979549561, valence: 1
+    },
+    {
+      name: 'pastoral', val: -0.04256596589, valence: false
+    }
+  ]
+};
